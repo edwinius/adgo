2e1c2de829e59d555daaa4ceaaf7747e04d61975
edwinius <edwinius@live.com>
2018-09-10 16:49:07 +0700

    Update global.js

--- app/global.js
+++ app/global.js
@@ -3,5 +3,4 @@ global.uri = 'http://www.apa.promo/';
 //global.uri = 'http://192.168.133.224/adgo/';
 //global.uri = 'http://127.0.0.1/adgo/';
 //global.uri = 'http://172.20.10.3/adgo/';
-global.appToken = '$2y$10$9ICvRTOVo7lwUwJlirwxgeEu4m/NOOPwlBOVqSzhTE6AD/sI1sjqO';
 global.appVersion = 'V1.09(9)'
